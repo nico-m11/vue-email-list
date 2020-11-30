6bd0317ae97ecf32fecb190e76fe949e7db3252d
Nicola <nicolamelito.11@gmail.com>
2020-11-30 18:23:45 +0100

    Ex completato

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const app = new Vue ({
+  el:'#root',
+  data: {
+    mails: []
+  },
+
+  mounted: function () {
+    for (var i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((result) => {
+        this.mails.push(result.data.response)
+      })
+    }
+    console.log(this.mails)
+  }
+})
